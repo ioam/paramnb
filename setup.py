05067d9258515ffee5a021b0001cd02d5bc207dd
jlstevens <jlstevens@ed.ac.uk>
2016-07-11 18:07:27 -0500

    Declared param>=1.4.0 as a paramnb dependency

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ except ImportError:
     from distutils.core import setup
 
 setup_args = {}
+install_requires = ['param>=1.4.0']
 
 setup_args.update(dict(
     name='paramnb',
     version="1.0.0",
+    install_requires = install_requires,
     description='Generate ipywidgets from Parameterized objects in the notebook',
     long_description=open('README.rst').read() if os.path.isfile('README.rst') else 'Consult README.rst',
     author= "philippjfr",
