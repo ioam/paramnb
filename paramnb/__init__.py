bb52e28c69876baf81e9a5e67ef23662d49513e1
Chris B <ceball@gmail.com>
2018-07-07 21:24:40 +0100

    Added examples command from pyct.

--- paramnb/__init__.py
+++ paramnb/__init__.py
@@ -485,3 +485,20 @@ class JSONInit(param.Parameterized):
                parameterized.set_param(**{name:value})
            except ValueError as e:
                warnobj.warning(str(e))
+
+
+##
+# make pyct's example/data commands available if possible
+from functools import partial
+try:
+    from pyct.cmd import copy_examples as _copy, fetch_data as _fetch, examples as _examples
+    copy_examples = partial(_copy, 'paramnb')
+    fetch_data = partial(_fetch, 'paramnb')
+    examples = partial(_examples, 'paramnb')
+except ImportError:
+    def _missing_cmd(*args,**kw): return("install pyct to enable this command (e.g. `conda install pyct` or `pip install pyct`)")
+    _copy = _fetch = _examples = _missing_cmd
+    def _err(): raise ValueError(_missing_cmd())
+    fetch_data = copy_examples = examples = _err
+del partial, _examples, _copy, _fetch
+##
